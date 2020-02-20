22137823376b64f71df9ade6355420b45ca869c5
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2020-02-20 11:05:29 +0330

    make day/night button a fab + ui refinments

--- lib/game.dart
+++ lib/game.dart
@@ -42,96 +42,80 @@ class GamePageState extends State<GamePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: isNight ? Colors.grey[900] : Colors.blueGrey[200],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isNight = !isNight;
+          });
+
+          if (isNight) {
+            widget.assetsAudioPlayer.playlistPlayAtIndex(Random().nextInt(4));
+          } else {
+            widget.assetsAudioPlayer.stop();
+          }
+        },
+        child: Icon(isNight ? Icons.wb_sunny : Icons.brightness_3),
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
+        mini: true,
+      ),
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  GridView.count(
-                    padding: EdgeInsets.fromLTRB(8, 8, 8, 250),
-                    crossAxisCount: 3,
-                    children: widget.players
-                        .map((player) => Opacity(
-                              opacity: player['status'] == 'dead' ? .3 : 1,
-                              child: InkWell(
-                                splashColor: Colors.red,
-                                onLongPress: () {
-                                  setState(() {
-                                    player['status'] = 'dead';
-                                  });
-                                },
-                                onTap: () {
-                                  if (player['status'] == 'dead') return;
-                                  setState(() {
-                                    player['status'] =
-                                        player['status'] == 'alive'
-                                            ? 'marked'
-                                            : 'alive';
-                                  });
-                                },
-                                child: Card(
-                                  color: player['status'] == 'marked'
-                                      ? Colors.grey[700]
-                                      : Colors.white10,
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        player['name'],
-                                        style: TextStyle(fontSize: 18),
-                                      ),
-                                      SizedBox(
-                                        height: 8,
-                                      ),
-                                      Text(
-                                        player['role'],
-                                        style: TextStyle(
-                                          color: Colors.white30,
-                                          fontSize: 18,
-                                        ),
-                                      ),
-                                    ],
+            GridView.count(
+              padding: EdgeInsets.fromLTRB(8, 8, 8, isNight ? 8 : 220),
+              crossAxisCount: 3,
+              children: widget.players
+                  .map((player) => Opacity(
+                        opacity: player['status'] == 'dead' ? .3 : 1,
+                        child: InkWell(
+                          splashColor: Colors.red,
+                          onLongPress: () {
+                            setState(() {
+                              player['status'] = 'dead';
+                            });
+                          },
+                          onTap: () {
+                            if (player['status'] == 'dead') return;
+                            setState(() {
+                              player['status'] = player['status'] == 'alive'
+                                  ? 'marked'
+                                  : 'alive';
+                            });
+                          },
+                          child: Card(
+                            color: player['status'] == 'marked'
+                                ? Colors.grey[700]
+                                : Colors.white10,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  player['name'],
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  player['role'],
+                                  style: TextStyle(
+                                    color: Colors.white30,
+                                    fontSize: 18,
                                   ),
                                 ),
-                              ),
-                            ))
-                        .toList(),
-                  ),
-                  Positioned(
-                    bottom: 10,
-                    left: 0,
-                    right: 0,
-                    child: Timer(),
-                  ),
-                ],
-              ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
             ),
-            RaisedButton(
-              onPressed: () {
-                setState(() {
-                  isNight = !isNight;
-                });
-
-                if (isNight) {
-                  widget.assetsAudioPlayer
-                      .playlistPlayAtIndex(Random().nextInt(4));
-                } else {
-                  widget.assetsAudioPlayer.stop();
-                }
-              },
-              padding: EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 24,
-              ),
-              child: Text(
-                isNight ? 'روز میشه' : 'شب میشه',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-              shape: StadiumBorder(),
+            if (! isNight) Positioned(
+              bottom: 10,
+              left: 0,
+              right: 0,
+              child: Timer(),
             ),
           ],
         ),
